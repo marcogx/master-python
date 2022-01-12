b015a2cfcb5ef6fada88ea158b65e352c24e9ae6
marcogx <marcogx419@gmail.com>
2022-01-11 18:31:20 -0800

    Code-demo wsgi server, web app & middleware

---
+++ wsgi/wsgi_demo.py
@@ -0,0 +1,30 @@
+"""
+Code-demo wsgi server, web app & middleware
+https://rahmonov.me/posts/what-the-hell-is-wsgi-anyway-and-what-do-you-eat-it-with/
+"""
+
+import time
+from wsgiref.simple_server import make_server
+
+
+def application(environ, start_response):
+    resp_body = str(time.time()) + '\n'.join([f'{key}: {val}' for key, val in sorted(environ.items())])
+    status = '200 OK'
+    resp_headers = [
+        ('Content-type', 'text/plain'),
+    ]
+    start_response(status, resp_headers)
+    return [resp_body.encode('utf-8')]
+
+
+class Reverseware:
+    def __init__(self, app):
+        self.wrapped_app = app
+
+    def __call__(self, environ, start_response):
+        return [data[::-1] for data in self.wrapped_app(environ, start_response)]
+
+
+if __name__ == '__main__':
+    server = make_server('localhost', 8001, app=Reverseware(application))
+    server.serve_forever()
